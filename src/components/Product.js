6e5baa81ac8e1b7b7543a17fd7c21a48c634e76a
Jack Norquist <jack.norquist@outlook.com>
2024-09-03 15:24:53 -0500

    product details added

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import ProductCatalogApi from '../../services/api';
+import ImageCarousel from './ImageCarousel'
 require('dotenv').config();
 
 
@@ -22,12 +23,29 @@ function Product({productId}) {
 
 
   return (
-    <div className="App">
+    <div className="productContainer">
       <h1>{productState.name}</h1>
       <p>{productState.description}</p>
-      <div className='product-images'>
+      <div className='productImages'>
+        <ImageCarousel images={productState.images}/>
+      </div>
+      <div className='descriptionContainer'>
+        <h6>Description</h6>
+        <p>{productState.description}</p>
+      </div>
+      <div className='colorsContainer'>
 
+      </div>
+      <div className='texturesContainer'>
 
+      </div>
+      <div className='sizesContainer'>
+
+      </div>
+      <div className='specContainer'>
+        <a href={`${productState.spec_sheet}`}>
+          <div className='specSheetContainer'>Spec Sheet</div>
+        </a>
       </div>
 
     </div>
