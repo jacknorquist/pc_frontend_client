529e3187cd19a185f63b08c50e40e0d7cffa3cd2
Jack Norquist <jack.norquist@outlook.com>
2024-09-03 14:53:06 -0500

    color image logic implemented

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const TOKEN = process.env.API_KEY
 
 function ImageCarousel({images}) {
-  const [imageState, setImageState] = useState({images:images, index:0})
+  const [imageState, setImageState] = useState({images:images, index:0, colorImage:null})
 
 
 
@@ -15,15 +15,29 @@ function ImageCarousel({images}) {
     <div className="image-carousel">
         <img src={images[imageState.index]}></img>
         <div className='image-thumbnails'>
+          
+        {imageState.colorImage ? imageState.images.map((image, index)=>{
+          if(image?.color === imageState.colorImage && index !=imagesState.index){
+            return <img
+                        key={image.image_url}
+                        className='image-thumbnail'
+                        src={image.image_url}
+                        />
+          }
+          return null
+        })
+        :
         {imageState.images.map((image, index) => {
-            if (index !== imageState.index) {
-              return <img
-                          key={index}
-                          className='image-thumbnail'
-                          src={image} alt={`Thumbnail ${index}`} />;
-            }
-            return null; // Return null if the condition is not met
-          })}
+          if (index !== imageState.index) {
+            return <img
+                        key={index}
+                        className='image-thumbnail'
+                        src={image} alt={`Thumbnail ${index}`} />;
+          }
+          return null; // Return null if the condition is not met
+        })}
+        }
+
         </div>
     </div>
 
