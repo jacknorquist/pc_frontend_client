f32a6e98d876b25d0177f2ca53aa6992c3a400aa
Jack Norquist <jack.norquist@outlook.com>
2024-09-03 13:51:09 -0500

    imageState added and to ImageCarousel

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,36 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
-import ProductCatalogApi from '../../services/api';
 require('dotenv').config();
 
 
 const TOKEN = process.env.API_KEY
 
-function ImageCarousel({productId}) {
-  const [productState, setProductState] = useState(null)
-  const [imageState, setImageState] = useState(null)
+function ImageCarousel({images}) {
+  const [imageState, setImageState] = useState({images:images, index:0})
 
-  useEffect(() => {
-    //fetch product
-    async function fetchProduct() {
-      const product = ProductCatalogApi.getProduct(TOKEN, productId)
-      setProductState(productState=product)
-    }
-    fetchProduct();
-  });
 
 
   return (
-    <div className="App">
-      <h1>{productState.name}</h1>
-      <p>{productState.description}</p>
-      <div className='product-images'>
-
-
-      </div>
-
+    <div className="image-carousel">
+        <img src={images[imageState.index]}></img>
+        <div className='image-thumbnails'>
+        {imageState.images.map((image, index) => {
+            if (index !== imageState.index) {
+              return <img
+                          key={index}
+                          className='image-thumbnail'
+                          src={image} alt={`Thumbnail ${index}`} />;
+            }
+            return null; // Return null if the condition is not met
+          })}
+        </div>
     </div>
+
+
   );
 }
 
