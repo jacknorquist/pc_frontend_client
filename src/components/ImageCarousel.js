9fabd1ea43d8575265ca0bb3f156a3c306ee5eb3
Jack Norquist <jack.norquist@outlook.com>
2024-09-03 15:09:08 -0500

    return to all images button and state

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -9,36 +9,65 @@ const TOKEN = process.env.API_KEY
 function ImageCarousel({images}) {
   const [imageState, setImageState] = useState({images:images, index:0, colorImage:null})
 
+  function updateImageIndex(event){
+    setImageState(
+      imageState.images = imageState.images,
+      imageState.index = event.key,
+      colorImage = imageState.colorImage
+    )
+  }
+
+  function returnToAllImages(){
+    setImageState(
+      imageState.images = imageState.images,
+      imageState.index = 0,
+      imageState.colorImage = null
+    )
+  }
 
 
+
+  //if images state signals that a color has been clicked, show images connected
+  //to the color, else display all images
   return (
     <div className="image-carousel">
+      <div className='main-image'>
         <img src={images[imageState.index]}></img>
-        <div className='image-thumbnails'>
-          
+      </div>
+      <div className='image-thumbnails'>
         {imageState.colorImage ? imageState.images.map((image, index)=>{
           if(image?.color === imageState.colorImage && index !=imagesState.index){
             return <img
-                        key={image.image_url}
+                        key={index}
                         className='image-thumbnail'
                         src={image.image_url}
+                        onClick={updateImageIndex}
                         />
           }
           return null
         })
         :
-        {imageState.images.map((image, index) => {
+        imageState.images.map((image, index) => {
           if (index !== imageState.index) {
             return <img
                         key={index}
                         className='image-thumbnail'
-                        src={image} alt={`Thumbnail ${index}`} />;
+                        src={image} alt={`Thumbnail ${index}`}
+                        onClick={updateImageIndex}
+                        />
           }
           return null; // Return null if the condition is not met
-        })}
+        })
         }
+        {imageState.colorImage ?
+        <div className='show-all-image-btn'
+        onClick={returnToAllImages}>
+             Show All Images
+        </div>
+          :""}
 
         </div>
+
     </div>
 
 
